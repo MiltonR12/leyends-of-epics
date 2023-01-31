b60aa8601cc8da4e165c1e102501b2237b562e13
MiltonR12 <109563498+MiltonR12@users.noreply.github.com>
2023-01-30 23:25:04 -0400

    cupos llenos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,12 +15,12 @@ function Home() {
         <h1>Leyend Of Epics</h1>
         <p>Dime Legends se complace en invitarte a este nuevo torneo para que demuestren sus habilidad y asi fomentar el competitivo en Bolivia.</p>
         <h3>DEBEN INGRESAR AL GRUPO DE WHATSPAPP PARA SABER SU HORARIO</h3>
-        {/* <h1>EN MANTENIMIENTO XD</h1> */}
-        <button
+        {/* <button
           className={estilos.btnEnviar}
           onClick={() => navigate('/form')}
           
-        >INSCRIBIRSE</button>
+        >INSCRIBIRSE</button> */}
+        <h1>CUPOS LLENOS</h1>
       </div>
       <Redes />
       <Reglas />
